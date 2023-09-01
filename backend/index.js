df6d7d68e205168855e987516b444dbc1e4bbb8a
Subhadip De <subhadipde.habra@gmail.com>
2023-09-01 11:26:58 +0530

    reverted changes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,1 @@
-import express from "express";
-import dotenv from "dotenv";
-
-// import Connection from "./database/db.js";
-
-dotenv.config();
-
-const app = express();
-
-const PORT = 8000;
-
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+console.group("Hello World");
