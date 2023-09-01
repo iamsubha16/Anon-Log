b4290c16c5f3c124a2f3ba06b9bc9a783a418364
Subhadip De <subhadipde.habra@gmail.com>
2023-09-01 11:43:07 +0530

    made connection with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-// import Connection from "./database/db.js";
+import Connection from "./database/db.js";
 
 dotenv.config();
 
@@ -10,3 +10,8 @@ const app = express();
 const PORT = 8000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+Connection(USERNAME, PASSWORD);
