31b2ef742aa7c949dfd78eb3d3ca14f04844190e
Subhadip De <subhadipde.habra@gmail.com>
2023-09-01 11:06:08 +0530

    made express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.group("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+
+// import Connection from "./database/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
